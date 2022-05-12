0304ff4e07bced1b3403a3a10253c2d61b53a496
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-05-12 22:01:06 +0300

    RestAssured logger: WIP

--- src/main/java/com/epam/reportportal/restassured/ReportPortalRestAssuredLoggingFilter.java
+++ src/main/java/com/epam/reportportal/restassured/ReportPortalRestAssuredLoggingFilter.java
@@ -16,20 +16,37 @@
 
 package com.epam.reportportal.restassured;
 
+import com.epam.reportportal.listeners.ItemStatus;
 import com.epam.reportportal.message.ReportPortalMessage;
+import com.epam.reportportal.message.TypeAwareByteSource;
+import com.epam.reportportal.service.Launch;
 import com.epam.reportportal.service.ReportPortal;
+import com.epam.reportportal.service.step.StepReporter;
+import com.epam.reportportal.utils.files.Utils;
 import com.google.common.io.ByteSource;
 import io.restassured.filter.FilterContext;
 import io.restassured.filter.OrderedFilter;
+import io.restassured.http.Cookies;
+import io.restassured.http.Headers;
+import io.restassured.internal.support.Prettifier;
+import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 import io.restassured.specification.FilterableRequestSpecification;
 import io.restassured.specification.FilterableResponseSpecification;
 import org.apache.http.entity.ContentType;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static java.util.Optional.ofNullable;
 
 public class ReportPortalRestAssuredLoggingFilter implements OrderedFilter {
 
@@ -52,6 +69,8 @@ public class ReportPortalRestAssuredLoggingFilter implements OrderedFilter {
 			ContentType.IMAGE_WEBP.getMimeType()
 	));
 
+	private static final String REMOVED_TAG = "&lt;removed&gt;";
+
 	private final int order;
 	private final String logLevel;
 
@@ -63,6 +82,34 @@ public class ReportPortalRestAssuredLoggingFilter implements OrderedFilter {
 	@Override
 	public int getOrder() {
 		return order;
+	}
+
+	@Nonnull
+	private String convertHeaders(@Nullable Headers headers) {
+		return ofNullable(headers).map(nonnullHeaders -> StreamSupport.stream(nonnullHeaders.spliterator(), false).map(h -> {
+			String value = "Authorization".equals(h.getName()) ? REMOVED_TAG : h.getValue();
+			return h.getName() + ": " + value;
+		}).collect(Collectors.joining("\n"))).orElse("");
+	}
+
+	private String formatTextHeader(Headers headers, Cookies cookies) {
+		String result = "";
+		String headersString = convertHeaders(headers);
+		if (!headersString.isEmpty()) {
+			result += "\n\n**Headers**\n" + headersString;
+		}
+		String cookiesString = ofNullable(cookies).map(Cookies::toString).orElse("");
+		if (!cookiesString.isEmpty()) {
+			result += "\n\n**Cookies**\n" + cookiesString;
+		}
+		return result;
+	}
+
+	private String formatTextEntity(Headers headers, Cookies cookies, String body, String contentType) {
+		String result = formatTextHeader(headers, cookies);
+		return ofNullable(body).map(b -> result + "\n\n**Body**\n```\n" + new Prettifier().prettify(body,
+				Parser.fromContentType(contentType)
+		) + "\n```").orElse(result);
 	}
 
 	private void attachAsBinary(String message, byte[] attachment, String contentType) {
@@ -78,18 +125,71 @@ public class ReportPortalRestAssuredLoggingFilter implements OrderedFilter {
 		String rqContent = ContentType.parse(request.getContentType()).getMimeType();
 
 		if (TEXT_CONTENT_TYPES.contains(rqContent)) {
-
+			ReportPortal.emitLog(logText + formatTextEntity(request.getHeaders(), request.getCookies(), request.getBody(), rqContent),
+					logLevel,
+					new Date()
+			);
 		} else if (IMAGE_CONTENT_TYPES.contains(rqContent)) {
-
+			attachAsBinary(logText, request.getBody(), rqContent);
 		} else if (MULTIPART_TYPE.equals(rqContent)) {
+			if (request.getMultiPartParams().isEmpty()) {
+				ReportPortal.emitLog(logText + formatTextHeader(request.getHeaders(), request.getCookies()), logLevel, new Date());
+				return;
+			}
 
+			StepReporter sr = ofNullable(Launch.currentLaunch()).map(Launch::getStepReporter).orElse(null);
+			ofNullable(sr).ifPresent(r -> r.sendStep(ItemStatus.INFO, logText));
+			Date currentDate = new Date();
+			ReportPortal.emitLog(formatTextHeader(request.getHeaders(), request.getCookies()), logLevel, currentDate);
+			request.getMultiPartParams().forEach(it -> {
+				Date myDate = new Date(currentDate.getTime() + 1);
+				String partMimeType = it.getMimeType();
+				if (TEXT_CONTENT_TYPES.contains(partMimeType)) {
+					ReportPortal.emitLog(
+							"**Body part**\n" + new Prettifier().prettify(it.getContent().toString(),
+									Parser.fromContentType(it.getMimeType())
+							),
+							logLevel,
+							myDate
+					);
+				} else {
+					Object body = it.getContent();
+					if (body != null) {
+						if (body instanceof File) {
+							try {
+								TypeAwareByteSource file = Utils.getFile((File) body);
+								attachAsBinary("**Body part**\n" + file.getMediaType(), file.read(), file.getMediaType());
+							} catch (IOException exc) {
+								ReportPortal.emitLog("**Body part**\nUnable to read file: " + exc.getMessage(), "ERROR", currentDate);
+							}
+						} else {
+							attachAsBinary("**Body part**\n" + rqContent, (byte[]) body, rqContent);
+						}
+					} else {
+						ReportPortal.emitLog("**Body part**\nNULL", logLevel, currentDate);
+					}
+				}
+			});
+			ofNullable(sr).ifPresent(StepReporter::finishPreviousStep);
 		} else {
 			attachAsBinary(logText, request.getBody(), rqContent);
 		}
 	}
 
 	private void emitLog(Response response) {
+		if (response == null) {
+			ReportPortal.emitLog("NULL response from RestAssured", logLevel, new Date());
+			return;
+		}
 
+		String logText = "**<<< RESPONSE**" + "\n" + response.getStatusLine();
+		String mimeType = ContentType.parse(response.getContentType()).getMimeType();
+		if (TEXT_CONTENT_TYPES.contains(mimeType)) {
+			logText += formatTextEntity(response.getHeaders(), response.getDetailedCookies(), response.getBody().asString(), mimeType);
+			ReportPortal.emitLog(logText, logLevel, new Date());
+		} else {
+			attachAsBinary(logText, response.getBody().asByteArray(), mimeType);
+		}
 	}
 
 	@Override
